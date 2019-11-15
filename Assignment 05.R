0d9a53524402cabd568d5e2ea46b7614eff12cd4
Benjamin Szmodis <bls223@lehigh.edu>
2019-11-15 01:57:36 -0500

    R Project
    Added CSV and Created R Script to read GDP.CSV

---
+++ Assignment 05.R
@@ -0,0 +1,15 @@
+rm(list=ls(all=TRUE))
+cat("\014") 
+if (!require("data.table")) install.packages("data.table")
+library("data.table")
+header <- read.table("GDP.csv", header = TRUE,
+                     sep=",", nrow = 1)
+DF <- fread("GDP.csv", skip=1, sep=",",
+            header=FALSE, data.table=FALSE)
+setnames(DF, colnames(header))
+rm(header)
+
+#Check
+View(DF)
+str(DF)
+summary(DF)
